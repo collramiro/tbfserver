a4b8e752be751cd41e831aecfb3ea7d4c1f664bb
Ramiro Coll Doñetz <coll.ramiro@gmail.com>
2016-12-28 17:33:01 -0300

    Rate logic

--- cloud/main.js
+++ cloud/main.js
@@ -32,31 +32,33 @@ Parse.Cloud.define("rateVideo", function(request, response) {
 	var query = new Parse.Query("Rate");
 	query.equalTo("videoObjectId", request.params.videoObjectId);
 	query.equalTo("userObjectId", request.params.userObjectId);
-	query.first({
-		success: function(object) {
-	// Successfully retrieved the object.
-	response.success(object);
-},
-error: function(error) {
-	var Rate = Parse.Object.extend("Rate");
-	var rate = new Rate();
 
-	rate.set("videoObjectId", request.params.videoObjectId);
-	gameScore.set("userObjectId", request.params.userObjectId);
+	query.find({
+		success: function(results) {
+			if (results.length > 0) {
+				var rate = results[0];
+				response.success('Already Rated: ' + rate.id);
+			} else {
+				var Rate = Parse.Object.extend("Rate");
+				var rate = new Rate();
 
-	gameScore.save(null, {
-		success: function(gameScore) {
-    // Execute any logic that should take place after the object is saved.
-    alert('New rate created with objectId: ' + gameScore.id);
-},
-error: function(gameScore, error) {
-    // Execute any logic that should take place if the save fails.
-    // error is a Parse.Error with an error code and message.
-    alert('Failed to create new rate, with error code: ' + error.message);
-}
-});
-}
-});
+				rate.set("videoObjectId", request.params.videoObjectId);
+				rate.set("userObjectId", request.params.userObjectId);
+
+				rate.save(null, {
+					success: function(rate) {
+						response.success('New rate created with objectId: ' + rate.id);
+					},
+					error: function(rate, error) {
+						response.error('Failed to create new rate, with error code: ' + error.message);
+					}
+				});
+			};
+		},
+		error: function(error) {
+			alert("Error: " + error.code + " " + error.message);
+		}
+	});
 });
 
 
@@ -67,18 +69,18 @@ Parse.Cloud.define("unrateVideo", function(request, response) {
 	query.first({
 		success: function(object) {
 
-object.destroy({
-  success: function(myObject) {
+			object.destroy({
+				success: function(myObject) {
     // The object was deleted from the Parse Cloud.
-  },
-  error: function(myObject, error) {
+},
+error: function(myObject, error) {
     // The delete failed.
     // error is a Parse.Error with an error code and message.
-  }
-});
-},
-error: function(error) {
-	response.error("Error: " + error.code + " " + error.message);
 }
 });
+		},
+		error: function(error) {
+			response.error("Error: " + error.code + " " + error.message);
+		}
+	});
 });
