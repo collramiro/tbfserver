6179649a26dd1dfda91f724007810894ad7298f4
Ramiro Coll Doñetz <coll.ramiro@gmail.com>
2016-12-28 18:22:24 -0300

    WIP: Rate functions

--- cloud/main.js
+++ cloud/main.js
@@ -3,31 +3,6 @@ Parse.Cloud.define('hello', function(req, res) {
 	res.success('Hi Ramiro');
 });
 
-
-Parse.Cloud.define('hello2', function(req, res) {
-	res.success('Hi Ramiro2');
-});
-
-Parse.Cloud.define("testCreate", function(request, response) {
-	var Rate = Parse.Object.extend("Rate");
-	var rate = new Rate();
-
-	rate.set("videoObjectId", "testObjectId");
-	rate.set("userObjectId", "userObjectId");
-
-	rate.save(null, {
-		success: function(rate) {
-    // Execute any logic that should take place after the object is saved.
-    response.success('New rate created with objectId: ' + rate.id);
-},
-error: function(rate, error) {
-    // Execute any logic that should take place if the save fails.
-    // error is a Parse.Error with an error code and message.
-    response.error('Failed to create new rate, with error code: ' + error.message);
-}
-});
-});
-
 Parse.Cloud.define("rateVideo", function(request, response) {
 	var query = new Parse.Query("Rate");
 	query.equalTo("videoObjectId", request.params.videoObjectId);
@@ -42,8 +17,17 @@ Parse.Cloud.define("rateVideo", function(request, response) {
 				var Rate = Parse.Object.extend("Rate");
 				var rate = new Rate();
 
-				rate.set("videoObjectId", request.params.videoObjectId);
-				rate.set("userObjectId", request.params.userObjectId);
+				var User = Parse.Object.extend("_User");
+				var user = new User();
+				user.id = request.params.userObjectId;
+				rate.set("user", user);
+				rate.set("userObjectId", user.id);
+
+				var Video = Parse.Object.extend("Video");
+				var video = new Video();
+				video.id = request.params.videoObjectId;
+				rate.set("video", video);
+				rate.set("videoObjectId", video.id);
 
 				rate.save(null, {
 					success: function(rate) {
@@ -66,18 +50,21 @@ Parse.Cloud.define("unrateVideo", function(request, response) {
 	var query = new Parse.Query("Rate");
 	query.equalTo("videoObjectId", request.params.videoObjectId);
 	query.equalTo("userObjectId", request.params.userObjectId);
-	query.first({
-		success: function(object) {
-
-			object.destroy({
-				success: function(myObject) {
-    // The object was deleted from the Parse Cloud.
-},
-error: function(myObject, error) {
-    // The delete failed.
-    // error is a Parse.Error with an error code and message.
-}
-});
+	query.find({
+		success: function(results) {
+			if (results.length > 0) {
+				var rate = results[0];
+				rate.destroy({
+					success: function(rate) {
+						response.success('Rate removed with objectId: ' + rate.id);
+					},
+					error: function(rate, error) {
+						response.error('Failed to remove rate, with error code: ' + error.message);
+					}
+				});
+			} else {
+				response.error("Not found rate object");
+			};
 		},
 		error: function(error) {
 			response.error("Error: " + error.code + " " + error.message);
